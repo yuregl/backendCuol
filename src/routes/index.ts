4451ca1f0aabf09a21b7373882a4b6a9911bceb6
yuregl <yuri.galdino@vendaecia.com>
2021-12-04 21:56:02 -0300

    feat: create function routes

---
+++ src/routes/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+async function exportApp() {
+  const app = express();
+  app.use(morgan("dev"));
+  app.use(cors());
+  app.use(express.json());
+  app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+      if (err instanceof Error) {
+        return response.status(400).json({
+          error: err.message,
+        });
+      }
+
+      return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error",
+      });
+    }
+  );
+  return app;
+}
+
+export { exportApp };
