58799caa1e00bfaa3a78a2a966809082e2f07730
yuregl <yuri.galdino@vendaecia.com>
2021-12-05 21:06:41 -0300

    feat: added routes cities

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,14 @@ import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import { createCitiesRouter } from "./CitiesRoutes";
+
 async function exportApp() {
   const app = express();
   app.use(morgan("dev"));
   app.use(cors());
   app.use(express.json());
+  app.use(createCitiesRouter());
   app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
       if (err instanceof Error) {
