fdc4329f26a746964d1afab3082810880814dedf
yuregl <yuri.galdino@vendaecia.com>
2021-12-06 23:39:51 -0300

    feat: added ClientsRoutes to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response } from "express";
 import morgan from "morgan";
 import cors from "cors";
 
 import { createCitiesRouter } from "./CitiesRoutes";
+import { createClientRouter } from "./ClientsRoutes";
 
 async function exportApp() {
   const app = express();
@@ -10,20 +11,19 @@ async function exportApp() {
   app.use(cors());
   app.use(express.json());
   app.use(createCitiesRouter());
-  app.use(
-    (err: Error, request: Request, response: Response, next: NextFunction) => {
-      if (err instanceof Error) {
-        return response.status(400).json({
-          error: err.message,
-        });
-      }
-
-      return response.status(500).json({
-        status: "error",
-        message: "Internal Server Error",
+  app.use(createClientRouter());
+  app.use((err: Error, request: Request, response: Response) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
       });
     }
-  );
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  });
   return app;
 }
 
