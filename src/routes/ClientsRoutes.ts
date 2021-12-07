e41db1127657634dfb7403a0860fbb2ae8ccabea
yuregl <yuri.galdino@vendaecia.com>
2021-12-06 23:39:20 -0300

    feat: Create ClientsRoutes

---
+++ src/routes/ClientsRoutes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { getCustomRepository } from "typeorm";
+import { ClientsController } from "../controllers/ClientController";
+import { ClientsService } from "../services/ClientsServices";
+import { ClientsRepositories } from "../repositories/ClientsRepositories";
+
+import { CitiesService } from "../services/CitiesServices";
+import { CitiesRepositories } from "../repositories/CitiesRepositories";
+
+const routesClient = Router();
+
+function createClientRouter() {
+  const citiesRepositories = getCustomRepository(CitiesRepositories);
+  const citiesService = new CitiesService(citiesRepositories);
+
+  const clientsRepositories = getCustomRepository(ClientsRepositories);
+  const clientsService = new ClientsService(clientsRepositories, citiesService);
+  const clientsController = new ClientsController(clientsService);
+
+  routesClient.post("/clients/new", clientsController.handleCreateClient);
+
+  return routesClient;
+}
+
+export { createClientRouter };
